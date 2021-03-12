a0860283516d340dc9032f5bedcc1b8a625b3f30
lis2612 <lyamin.ilya.s@gmail.com>
2021-03-12 11:13:51 +0400

    new calculating algorythm and command-line args

--- test_stl.py
+++ test_stl.py
@@ -1,49 +1,93 @@
 import numpy
 from stl import mesh
-from mpl_toolkits import mplot3d
-from matplotlib import pyplot
+# from mpl_toolkits import mplot3d
+# from matplotlib import pyplot
 import sys
 import argparse
+import stl
+import math
 
-def createParser ():
+
+def createParser():
     parser = argparse.ArgumentParser()
-    #Добавляем аргументы
-    parser.add_argument ('-f','--file', nargs='?',help='Filename STL model')
-    parser.add_argument('-p','--price',nargs='?',default=10,help='Price of gram 3d-printing model')
+    # Добавляем аргументы
+    parser.add_argument('file', nargs='?', help='Filename STL model')
+    parser.add_argument('-p', '--price',   nargs='?', type=float, default=10,
+                        help='Price of gram 3d-printing model. Default=10')
+    parser.add_argument('-d', '--density', nargs='?', type=float, default=1.25,
+                        help='Density of material. Default=1.25')
+    parser.add_argument('-i', '--indensity', nargs='?', type=float, default=0.2,
+                        help='Density of infill pattern. Default=0.2')
+    parser.add_argument('-t', '--thicken', nargs='?', type=float, default=1,
+                        help='Shell thicken. Default=1')
     return parser
 
-def calculate(f,gramm_price):
-    # Create a new plot
-    figure = pyplot.figure()
-    axes= mplot3d.Axes3D(figure)
+def getObjHeight(obj):
+    return obj.z.max()-obj.z.min()
 
-    # Load the STL files and add the vectors to the plot
-    your_mesh = mesh.Mesh.from_file(f)
-    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
+# def toDecart(r,theta,phi):
+#     x=round(r*math.sin(theta)*math.cos(phi),5)
+#     y=round(r*math.sin(theta)*math.sin(phi),5)
+#     z=round(r*math.cos(theta),5)
+#     # print("To Decart:",x,y,z)
+#     return x,y,z
 
-    # Auto scale to the mesh size
-    #scale = your_mesh.points.flatten()
-    #axes.auto_scale_xyz(scale, scale, scale)
+# def toSphere(x,y,z):
+#     r=math.sqrt(x*x+y*y+z*z)
+#     theta=math.acos(z/r)
+#     phi=math.atan(y/x)
+#     # print(x,y,z)
+#     # print("To Sphere:",r,theta,phi)
+#     # toDecart(r,theta,phi)
+#     return r,theta,phi
+    
 
-    # Show the plot to the screen
-    #pyplot.show()
+    # Функция расчета стоимости печати изделия
+    # f - STL файл модели
+    # gramm_price - стоимость печати за грамм готового изделия
+    # density - плотность материала
+    # infill_density - плотность рисунка заполнения
+    # shell_thicken - толщина стенок
+def calculate(f, gramm_price, density,infill_density,shell_thicken):
+    
+    # Load the STL file
+    your_mesh = mesh.Mesh.from_file(f, remove_duplicate_polygons=True, remove_empty_areas=True, calculate_normals=True)
+    # Get model characteristics
+    volume, vmass, cog, inertia = your_mesh.get_mass_properties_with_density(density)
+    # print("Volume     = {:.2f} cm^3".format(volume/1000))
+    # print("Mass       = {:.2f} g".format((vmass/1000),1))
+    # print("Price      = {:.2f} RUR" .format(vmass/1000*gramm_price))
+    
+    #Calculate infill volume   
+    kscale=1-2*shell_thicken/getObjHeight(your_mesh)
+    your_mesh.data['vectors']*=kscale
+    infill_volume, infill_vmass, infill_cog, infill_inertia = your_mesh.get_mass_properties_with_density(density*infill_density)
+    # print("Volume     = {:.2f} cm^3".format(infill_volume/1000))
+    # print("Mass       = {:.2f} g".format((infill_vmass/1000)))
+    # print("Price      = {:.2f} RUR" .format(infill_vmass/1000*gramm_price))
+    
 
-    volume, cog, inertia = your_mesh.get_mass_properties()
-    volume=volume/1000
-    print("Volume                                  = {:.2f} cm^3".format(volume))
-    mass=round(volume*1.25)
-    print("Mass                                    = {:.2f} g".format(mass))
-    price=mass*gramm_price
-    print("Price                                   ={:.2f} RUR" .format(price))
-    #help('FORMATTING')
+    # Calculate mass and price of detal
+    shell_mass=(volume-infill_volume)*density
+    detal_mass=infill_vmass+shell_mass
+    detal_price=detal_mass/1000*gramm_price
+
+    print("********************** INVOICE ***************************")
+    print("Mass       = {:.2f} g".format((detal_mass/1000)))
+    print("Price      = {:.2f} RUR" .format(detal_price))
+    print("**********************************************************")
+
+    # your_mesh.save('new.stl')
+                
+
 
 if __name__ == "__main__":
     parser = createParser()
     namespace = parser.parse_args()
-    print (namespace)
-
-    if namespace.file==None:
+   
+    if namespace.file == None:
         parser.print_help()
+        
     if namespace.file:
-        calculate(namespace.file,float(namespace.price))
-    
+        calculate(namespace.file, namespace.price, namespace.density,namespace.indensity,namespace.thicken)
+        
